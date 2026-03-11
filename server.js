1db350a231899582cf229c83451d23441a91484a
vnanev-hub <vnanev@gmail.com>
2026-03-11 21:27:03 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Тук казваме на сървъра да търси файлове в папка 'public'
-app.use(express.static('public'));
+// Уверяваме се, че всички файлове са достъпни
+app.use(express.static(path.join(__dirname)));
 
-// ТОВА Е КЛЮЧОВИЯТ РЕД: Той казва на сървъра да сервира index.html при отваряне
+// Това казва: когато някой отвори началната страница, му покажи index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Слушане на порт (Render сам си задава порта чрез process.env.PORT)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
